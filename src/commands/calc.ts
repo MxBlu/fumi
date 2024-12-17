a69e18decf30fea3493d55e12ec7abdf0b2af31d
Jumail <jmundekkat@rev.com.au>
2024-12-17 15:03:03 +1100

    Add calc command

---
+++ src/commands/calc.ts
@@ -0,0 +1,47 @@
+import { Logger, LogLevel } from "bot-framework";
+import { CommandBuilder, CommandProvider, sendCmdReply } from "bot-framework/discord";
+import { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
+import { evaluate } from "mathjs";
+
+import { replyWithEmbed } from "fumi/utils/bot_utils.js";
+
+export class CalcCommand implements CommandProvider<ChatInputCommandInteraction> {
+  logger: Logger;
+
+  constructor() {
+    this.logger = new Logger("Commands.Calc");
+  }
+
+  provideCommands(): CommandBuilder[] {
+    return [
+      new SlashCommandBuilder()
+        .setName("calc")
+        .setDescription("Calculate a value")
+        .addStringOption(
+          new SlashCommandStringOption()
+          .setName("math")
+          .setDescription("Calculations")
+          .setRequired(true)) as unknown as CommandBuilder
+    ]
+  }
+
+  provideHelpMessage(): string {
+    return "/calc <math> - Calculate a value";
+  }
+
+  async handle(interaction: ChatInputCommandInteraction): Promise<void> {
+    const math = interaction.options.getString('math');
+
+    try {
+      const result = evaluate(math);
+
+      replyWithEmbed(interaction, [
+          { name: math, value: `${result}` }
+        ], this.logger, `Answered math equation: ${math}`, LogLevel.TRACE);
+    } catch (e) {
+      this.logger.debug(`Equation could not be calculated: ${math}`);
+      sendCmdReply(interaction, `Invalid equation`, this.logger, LogLevel.DEBUG, { ephemeral: true });
+      return;
+    }
+  }
+}
--- src/modules/fumi.ts
+++ src/modules/fumi.ts
@@ -1,6 +1,8 @@
+import { Dependency } from "bot-framework";
 import { ClientOptionsWithoutIntents, DiscordBot } from "bot-framework/discord";
 import { BitFieldResolvable, GatewayIntentsString } from "discord.js";
 import { EightBallCommand } from "fumi/commands/8ball.js";
+import { CalcCommand } from "fumi/commands/calc.js";
 import { ChooseCommand } from "fumi/commands/choose.js";
 import { ConvertCommand } from "fumi/commands/convert.js";
 import { CurrencyConversionDependency } from "fumi/utils/conversion.js";
@@ -16,6 +18,8 @@ class _Fumi extends DiscordBot {
     await CurrencyConversionDependency.await();
 
     await super.init(discordToken);
+    // Mark bot as ready
+    FumiDependency.ready();
   }
 
   protected getHelpMessage(): string {
@@ -26,7 +30,9 @@ class _Fumi extends DiscordBot {
     this.providers.push(new ChooseCommand());
     this.providers.push(new EightBallCommand());
     this.providers.push(new ConvertCommand());
+    this.providers.push(new CalcCommand());
   }
 }
 
 export const Fumi = new _Fumi();
+export const FumiDependency = new Dependency("Fumi");
